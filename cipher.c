2353522d1549eb998a95c17474063e03f290be92
Matthew McNeil <c3278520@uon.edu.au>
2019-04-05 02:42:56 +0000

    Adding Base

--- cipher.c
+++ cipher.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
 
-ghjk
+int main()
+{
+    
+    /*int choice;
+    
+    printf("Please choose an option : \n");
+    printf("\n");
+    printf("1. Encryption of a message \n");
+    printf("2. Decryption of a message \n");
+    
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            printf("Choose Cipher Type: \n \n");
+            printf("1. Encryption of a message with a rotation cipher \n");
+            printf("2. Decryption of a message encrypted with a rotation cipher \n");     
+        
+    } */
+   
+    
+    
+}
