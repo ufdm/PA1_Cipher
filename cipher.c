1840c3e5f4b1d651b867f8e32f085d38aee78569
Matthew McNeil <c3278520@uon.edu.au>
2019-04-05 02:45:52 +0000

    fgh

--- cipher.c
+++ cipher.c
@@ -2,7 +2,24 @@
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+    
+    /*int choice;
+    
+    printf("Please choose an option : \n");
+    printf("\n");
+    printf("1. Encryption of a message \n");
+    printf("2. Decryption of a message \n");
+    
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            printf("Choose Cipher Type: \n \n");
+            printf("1. Encryption of a message with a rotation cipher \n");
+            printf("2. Decryption of a message encrypted with a rotation cipher \n");     
+        
+    } */
+   
+    
+    
 }
