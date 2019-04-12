97772b2fdca5cfa44be066126d185f6db147153d
Matthew McNeil <c3278520@uon.edu.au>
2019-04-12 02:21:21 +0000

    Small tests

--- cipher.c
+++ cipher.c
@@ -2,24 +2,32 @@
 
 int main()
 {
+    char msg[] = "This is just a bunch of if satements isnt it?";
     
+    
+    //int choice;
+    /* Notes
+    use an arry to store the message? and char to save decrypted/encrypted messages
+    */
     /*int choice;
     
     printf("Please choose an option : \n");
     printf("\n");
     printf("1. Encryption of a message \n");
-    printf("2. Decryption of a message \n");
+    printf("2. Decryption of a message \n"); 
     
-    scanf("%d", &choice); */
+    scanf("%d", &choice);
     
+    // 
     switch(choice) {
         case 1:
             printf("Choose Cipher Type: \n \n");
             printf("1. Encryption of a message with a rotation cipher \n");
             printf("2. Decryption of a message encrypted with a rotation cipher \n");     
         
-    } 
+    } */
    
     
-    
+    printf("Integer = %d \n", choice);
+    return 0;
 }
