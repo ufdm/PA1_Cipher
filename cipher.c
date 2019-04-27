f64e1d9251b875cd80e02446b652d0e628af9c2d
Matthew McNeil <c3278520@uon.edu.au>
2019-04-27 13:10:26 +0000

    Capitals

---
+++ cipher.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    char msg[] = "This is just a bunch of if satements isnt it?";
+    
+    for(int i = 1, msg[i] !=0/*check rules for chars*/ ) {
+        msg[i] = convert
+    }
+    //int choice;
+    /* Notes
+    use an arry to store the message? and char to save decrypted/encrypted messages
+    */
+    /*int choice;
+    
+    printf("Please choose an option : \n");
+    printf("\n");
+    printf("1. Encryption of a message \n");
+    printf("2. Decryption of a message \n"); 
+    
+    scanf("%d", &choice);
+    
+    // 
+    switch(choice) {
+        case 1:
+            printf("Choose Cipher Type: \n \n");
+            printf("1. Encryption of a message with a rotation cipher \n");
+            printf("2. Decryption of a message encrypted with a rotation cipher \n");     
+        
+    } */
+   
+    
+    printf("Integer = %d \n", choice);
+    return 0;
+}
+
+char convert_to_caps (msg[]) {
+    if (msg[i] > 97) {
+            msg[i] = msg [i] - 32;
+    }    
+}
