2aeeb5c2848b480d85f9042f6511db7aae109bd1
Matthew McNeil <c3278520@uon.edu.au>
2019-06-07 04:50:03 +0000

    final

--- cipher.c
+++ cipher.c
@@ -1,40 +1,55 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h> //For the exit function
+#define MAX 500 //Size for the arrays
 //Declaring Functions
-
-int convertToCaps (int letter);
-int rotEncrypt(int key, int letter);
+int convertToCaps (int letter); 
+int rotEncrypt(int key, int tempN);
 
 int main()
 {
-    char msg[] = "Testing. Testing. Is this thing on?\n";
+    int choice; // The varible that holds the choice of the user for the switch menu
+    int key = 1;
     int tempN;
-    int key;
     
-    key = 1;
+    char msg[MAX]; //Declaring the array to hold the message
     
-           
-    for(int i = 0; msg[i] != '\0'; i++) {
-        printf("%c", msg[i]);
-        
-        if((msg[i] >= 65 && msg[i] <= 90) || (msg[i] >= 97 && msg[i] <= 122)) {            
-            tempN = msg[i];
-            tempN = convertToCaps(tempN);
-            tempN = rotEncrypt(key, tempN);
-            msg[i] = tempN;
-        }
-    }
-        
-    for(int i = 0; msg[i] != '\0'; i++) {
-        printf("%c", msg[i]);
-    }
+    //Menu Selections displayed to the user
+    printf("Please choose an option : \n");
     printf("\n");
-
+    printf("1. Encryption of a message \n");
+    printf("2. Decryption of a message \n");
+    printf("-1 to Exit\n");
     
-    return 0;
-}
+    //while(choice != -1) {
+        scanf("%d\n", &choice); //Scanner the choice inputed
+        switch(choice) {
+            case 1:
+                //Reads the message entered.
+                for(int i = 0; msg[i] != '\0'; i++) {
+                    scanf("%[^\n]c", &msg[i]);
+                }
+                                
+                printf("Here is the encrypted message.\n");
+                for(int i = 0; msg[i] != '\0'; i++) {              
+                    tempN = msg[i];
+                    msg[i] = rotEncrypt(key, tempN);
+                    printf("%c", msg[i]);
+                }
+                printf("\n");
+                    
+                break;
+        }
+    //}
 
+        
+    
+
+
+    return 0;   
+    
+}
+//Function tht coverts the number values of chars from lowercase to uppercase
 int convertToCaps (int letter) {
     if (letter > 97) {
             letter = letter - 32;
@@ -43,33 +58,16 @@ int convertToCaps (int letter) {
     return(letter);
 }
 
-int rotEncrypt(int key, int letter) {
-    letter = (letter + key);
-    
-    return (letter);    
+//Function for the encrypting of a message using a rotation cipher and provided key
+int rotEncrypt(int key, int tempN) {
+    if((tempN >= 65 && tempN <= 90) || (tempN >= 97 && tempN <= 122)) {        // Checking if it is a character    
+            tempN = convertToCaps(tempN);
+            tempN = (tempN + key);
+    }
+    return (tempN);
 }
 
 
 
 
 
-/*    int choice;
-    
-    printf("Please choose an option : \n");
-    printf("\n");
-    printf("1. Encryption of a message \n");
-    printf("2. Decryption of a message \n"); 
-    
-    scanf("%d", &choice);
-    
-    
-    switch(choice) {
-        case 1:
-            printf("Choose Cipher Type: \n \n");
-            printf("1. Encryption of a message with a rotation cipher \n");
-            printf("2. Decryption of a message encrypted with a rotation cipher \n");     
-        
-    } 
-   
-    
-    printf("Integer = %d \n", choice); */
